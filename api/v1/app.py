6115af3b744b4aafed15f8ab7ea23f871b98a1e5
Derhks <derhks@gmail.com>
2021-06-10 20:58:26 -0500

    Added route to display information about the anime viewed

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,10 +1,10 @@
 from flasgger import Swagger
-from flask import Flask
+from flask import Flask, render_template
 from flask_restful import Api
 
 
 app = Flask(__name__)
-api = Api(app)
+api = Api(app, prefix='/api/v1/')
 swagger = Swagger(app)
 
 
@@ -18,6 +18,21 @@ api.add_resource(FinishedAnimeId, '/finished-anime/<int:anime_viewed_id>')
 api.add_resource(Animes, '/animes/')
 api.add_resource(AnimeId, '/animes/<int:anime_id>')
 # End of endpoint initialization
+
+# Start Test
+from models.anime import Anime
+
+
+@app.route('/anime-list/', methods=['GET'], strict_slashes=False)
+def anime_list():
+    all_animes = Anime.query.all()
+    list_animes = []
+
+    for anime in all_animes:
+        list_animes.append(anime.to_dict())
+
+    return render_template('animes.html', list_animes=list_animes)
+# End Test
 
 
 if __name__ == '__main__':
